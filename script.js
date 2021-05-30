2f9461a579097c49d8cd40686f5d4d27e4d5c860
ManonLef <81025586+ManonLef@users.noreply.github.com>
2021-05-30 21:40:03 +0200

    added best of 5 rounds and endGame()

--- script.js
+++ script.js
@@ -1,12 +1,23 @@
 let playerScore = 0;  
 let computerScore = 0;
 let rounds = 1;
-let playerSelection;
 
 function computerPlay() {
-    let compChoice = ["rock", "paper", "scissors"]; // note to self: mind the brackets [] for math
+    let compChoice = ["rock", "paper", "scissors"]; 
     let computerSelection = (compChoice[Math.floor(Math.random() * compChoice.length)]);
     return(computerSelection);
+}
+
+function endGame() {
+    if (confirm("Would you like another attempt at beating R2's backend?")) {
+        playerScore = 0;  
+        computerScore = 0;
+        rounds = 1;
+        document.getElementById("results").innerHTML = "pick a weapon"
+    } else {
+        
+        document.getElementById("results").innerHTML = "you coward"
+    }
 }
 
 function playRound(playerSelection, computerSelection) {
@@ -18,37 +29,59 @@ function playRound(playerSelection, computerSelection) {
         rounds ++
 
         if (playerSelection === computerSelection) { 
-            computerScore++;
-            document.getElementById("results").innerHTML = "your objects collided mid-air, it's a tie. " + playerScore + " for you and " + computerScore + " for R2-D2.";   
+            document.getElementById("results").innerHTML = "you both picked " + playerSelection + ", it's a tie. " + playerScore + " for you and " + computerScore + " for R2-D2.";   
         } else if (playerSelection === "rock" && computerSelection === "paper") {
             computerScore++;
-            document.getElementById("results").innerHTML = "You lose! R2-D2 had some rock-eating paper apparently. The score is now " + playerScore + " for you and " + computerScore + " for R2-D2.";
+            if (computerScore !== 5) {
+                document.getElementById("results").innerHTML = "You lose! R2-D2 had some rock-eating paper apparently. The score is now " + playerScore + " for you and " + computerScore + " for R2-D2.";
+            } else {
+                document.getElementById("results").innerHTML = "you lose. The final score is " + playerScore + " for you and " + computerScore + " for R2-D2!";
+                endGame();
+            }
         } else if (playerSelection === "rock" && computerSelection === "scissors") {
             playerScore++;
-            document.getElementById("results").innerHTML = "You win! Those scissors stood no chance against your mighty rock. The score is now " + playerScore + " for you and " + computerScore + " for R2-D2.";
+            if (playerScore !== 5) {
+                document.getElementById("results").innerHTML = "You win! Those scissors stood no chance against your mighty rock. The score is now " + playerScore + " for you and " + computerScore + " for R2-D2.";
+            } else {
+                document.getElementById("results").innerHTML = "you win! The final score is " + playerScore + " for you and " + computerScore + " for R2-D2!";
+                endGame();
+            }
         } else if (playerSelection === "paper" && computerSelection === "rock") {
             playerScore++;
-            document.getElementById("results").innerHTML = "You win! This paper must have come from Uranus, it entirely blocked R2-D2's rock. The score is now " + playerScore + " for you and " + computerScore + " for R2-D2.";
+            if (playerScore !== 5) {
+                document.getElementById("results").innerHTML = "You win! This paper must have come from Uranus, it entirely blocked R2-D2's rock. The score is now " + playerScore + " for you and " + computerScore + " for R2-D2.";
+            } else {
+                document.getElementById("results").innerHTML = "you win! The final score is " + playerScore + " for you and " + computerScore + " for R2-D2!";
+                endGame();
+            }   
         } else if (playerSelection === "paper" && computerSelection === "scissors") {
             computerScore++;
-            document.getElementById("results").innerHTML = "R2-D2 whips out his Swiss army knife and cuts your paper to pieces. Obvious loss. The score is now " + playerScore + " for you and " + computerScore + " for R2-D2.";
+            if (computerScore !== 5) {
+                document.getElementById("results").innerHTML = "R2-D2 whips out his Swiss army knife and cuts your paper to pieces. Obvious loss. The score is now " + playerScore + " for you and " + computerScore + " for R2-D2.";
+            } else {
+                document.getElementById("results").innerHTML = "you lose. The final score is " + playerScore + " for you and " + computerScore + " for R2-D2!";
+                endGame();
+            }
         } else if (playerSelection === "scissors" && computerSelection === "rock") {
             computerScore++;
-            document.getElementById("results").innerHTML = "You lose! Those blades got pretty roughed up by R2D2's rock. The score is now " + playerScore + " for you and " + computerScore + " for R2-D2.";
+            if (computerScore !== 5) {
+                document.getElementById("results").innerHTML = "You lose! Those blades got pretty roughed up by R2D2's rock. The score is now " + playerScore + " for you and " + computerScore + " for R2-D2.";
+            } else {
+                document.getElementById("results").innerHTML = "you lose. The final score is " + playerScore + " for you and " + computerScore + " for R2-D2!";
+                endGame();
+            }   
         } else if (playerSelection === "scissors" && computerSelection === "paper") {
             playerScore++;
-            document.getElementById("results").innerHTML = "You win! you cut R2's pathetic little piece of paper into world's most tiny violin and threw it back. The score is now " + playerScore + " for you and " + computerScore + " for R2-D2.";
+            if (playerScore !== 5) {
+                document.getElementById("results").innerHTML = "You win! you cut R2's pathetic little piece of paper into world's most tiny violin and threw it back. The score is now " + playerScore + " for you and " + computerScore + " for R2-D2.";
+            } else {
+                document.getElementById("results").innerHTML = "you win! The final score is " + playerScore + " for you and " + computerScore + " for R2-D2!";
+                endGame();
+            }   
         }  
     } 
 }
 
-function endGame() {
-    if (computerScore > playerScore) {
-        return("* GAME OVER * YOU LOSE! You got pulverized by a cute robot.")
-    } else {
-        return("* GAME OVER * YOU WIN! But beware of the sequel. Return of the astromech droids. You read that right... DroidS")
-    }
-}
 
 document.getElementById("rock").addEventListener("click", () => {
     playRound("rock",);
